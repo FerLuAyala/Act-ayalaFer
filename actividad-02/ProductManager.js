bcc484598d0d35b84099638fda9d54cd1fe5a3d8
FerLuAyala <fernanda.ayala@davinci.edu.ar>
2025-08-19 21:25:56 -0300

    actividad-03 // delete-update

--- actividad-02/ProductManager.js
+++ actividad-02/ProductManager.js
@@ -7,21 +7,21 @@ class ProductManager {
     this.products = products;
   }
 
-  
   async loadProducts() {
     try {
-      const data = await fs.readFile(this.path);
-      if (data) {
-        this.products = JSON.parse(data);
-      } else {
+      const data = await fs.readFile(this.path, "utf-8");
+
+      if (data.trim().length === 0) {
         this.products = [];
+      } else {
+        this.products = JSON.parse(data);
       }
     } catch (error) {
-      console.log(error);
+      console.log("Error al cargar productos:", error);
     }
   }
-  
-  async addProduct(nombre) {
+
+  async addProduct(nombre , precio , color) {
     await this.loadProducts();
     const nombreMayus = nombre.toUpperCase();
     if (this.products.some((p) => p.nombre === nombreMayus)) {
@@ -32,12 +32,15 @@ class ProductManager {
     this.products.push({
       id: id,
       nombre: nombreMayus,
+      precio: precio,
+      color: color
     });
 
     await this.saveProducts();
   }
 
-  getProducts() {
+  async getProducts() {
+    await this.loadProducts();
     return this.products;
   }
 
@@ -54,6 +57,32 @@ class ProductManager {
       return {};
     }
     return product;
+  }
+  async deleteProductByid(id) {
+    //leo os productos 
+    await this.loadProducts();   
+   //buscamos el id
+    const index =this.products.findIndex(p=> p.id==id);
+    //eliminar
+    if(index == -1){
+      return 'NO se encontro Id para eliminar'
+    } else {
+      this.products.splice(index, 1);
+      await this.saveProducts();
+      return index;
+    }
+  }
+
+  async updateProduct(id, newPrecio){
+    await this.loadProducts();   
+    const index =this.products.findIndex(p=> p.id==id);
+     if(index == -1){
+      return 'NO se encontro'
+    } 
+    this.products[index].precio = newPrecio;  
+    await this.saveProducts();
+    return index;
+
   }
 }
 
--- actividad-02/index.js
+++ actividad-02/index.js
@@ -1,11 +1,38 @@
+
+const http= require('http');
+const port = 3001;
+const server = http.createServer( (request, response) => {
+    const url = request.url;
+    const method = request.method;
+    
+    console.log(url, method);
+    if( url == '/'){
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        response.end('<h1>Hola desde Node!</h1>');
+    } else if ( url == '/users'){
+        response.writeHead(200, { 'Content-Type': 'application/json' });
+        response.end('JSON');
+    } else if ( url == '/products'){
+        response.writeHead(200, { 'Content-Type': 'application/json' });
+        response.end('Productos');
+    } else {
+        response.writeHead(404, { 'Content-Type': 'text/html' });
+        response.end('<h1>404 | Not Found</h1>');
+    }
+
+});
+
+server.listen( port, () => {
+    console.log(`Servidor Web en el puerto ${port}`);
+})
+
 const ProductManager = require("./ProductManager");
 const p1 = new ProductManager();
-
 async function saveProducts() {
   //espera que se agregan todos los products y luego los guarda
-  await p1.addProduct("Plancha");
-  await p1.addProduct("Heladera");
-  await p1.addProduct("Microondas");
+  await p1.addProduct("Plancha" , 5000 , "rosa");
+  await p1.addProduct("Heladera" , 600 , "gris ");
+  await p1.addProduct("Microondas" , 7800, "negro");
 }
 
 
@@ -15,10 +42,21 @@ async function accionesProduct() {
   const data = p1.getProducts();
   console.table(data);
   const producto = await p1.getProductById(
-    "37badfd9-8169-4824-9851-1e32d412f9ae"
+    "cdb2cb8e-3cf6-4b19-9ae3-ed68c5a9f7ee"
   );
   console.log("id encontrado:");
   console.table(producto);
+
+  const actualizado = await p1.updateProduct("802f600d-d798-4c71-b086-d5e9e93f0fd3" , precio= 180800);
+  console.log("producto actualizado");
+  console.table(actualizado);
+  const dataN = await p1.getProducts();
+  console.table(dataN);
+  const eliminado =await p1.deleteProductByid("320e321d-8d76-404a-93d1-43694deeb337");
+  console.log(eliminado);
+  
 }
+
+
 
 accionesProduct();
